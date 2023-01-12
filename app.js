656d810b59fae1a24baea6ee9788630c5a711b04
nikhil-vp2811 <nikhil@valupeitch.com>
2023-01-12 07:34:08 +0530

    nested react elemeent with h1,h2,h3 inside a div

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+
+
 const heading = React.createElement(
     "h1",
     {
-        id:"title"
+        id:"title",
+        key:"1"
     },
     "Heading 1"
 )
@@ -9,17 +15,28 @@ const heading = React.createElement(
 const heading2 = React.createElement(
     "h2",
     {
-        id:"title"
+        id:"title",
+        key:"2"
     },
-    "heading 2"
+    "Heading 2"
+)
+
+const heading3 = React.createElement(
+    "h3",
+    {
+        id:"title",
+        keys:"3"
+    },
+    "Heading 3"
 )
 
 const container = React.createElement(
     "div",
     {
-        id:"container"
+        id:"container",
+        key:"3"
     },
-    [heading, heading2]
+    [heading, heading2, heading3 ]
 )
 
 const root  = ReactDOM.createRoot(document.getElementById('root'));
